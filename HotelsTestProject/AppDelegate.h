20846764569c20326b4cc3ff29a0f312c5ebfe94
Artem Abramov <arthor@yandex.ru>
2015-09-16 10:21:21 +0300

    Initial Commit

---
+++ HotelsTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HotelsTestProject
+//
+//  Created by Artem Abramov on 16/09/15.
+//  Copyright © 2015 Artem Abramov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HotelsTestProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HotelsTestProject
+//
+//  Created by Artem Abramov on 16/09/15.
+//  Copyright © 2015 Artem Abramov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
